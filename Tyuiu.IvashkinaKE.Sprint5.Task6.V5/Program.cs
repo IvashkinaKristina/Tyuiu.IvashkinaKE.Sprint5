363989ffc07621f74af326d7dad6851133e4e196
Tyuiu.IvashkinaKE@outlook.com <Tyuiu.IvashkinaKE@outlook.com>
2023-11-26 13:44:50 +0500

    Task6

--- Tyuiu.IvashkinaKE.Sprint5.Task6.V5/Program.cs
+++ Tyuiu.IvashkinaKE.Sprint5.Task6.V5/Program.cs
@@ -16,7 +16,7 @@ namespace Tyuiu.IvashkinaKE.Sprint5.Task6.V5
             Console.Title = "Спринт #5 | Выполнила: Ивашкина К. Э.  | АСОиУБ-23-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #5                                                               *");
-            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
             Console.WriteLine("* Задание #6                                                              *");
             Console.WriteLine("* Вариант #5                                                              *");
             Console.WriteLine("* Выполнила: Ивашкина Кристина Эдуардовна | АСОиУБ-23-1                   *");
